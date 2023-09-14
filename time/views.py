d5d0ad8189b9b38faeaf2de2aa4c5f2a7794ef9d
pymute <134302465+pymute@users.noreply.github.com>
2023-09-14 17:52:31 +0500

    committed

--- time/views.py
+++ time/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# @api_view(['GET'])
+
 class BornView(APIView):
     def endpoint(request):
         if request.method == 'GET':
